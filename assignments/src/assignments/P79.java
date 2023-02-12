be401291d960d5e634e0776dac87d1f0d43756b9
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-02-12 12:49:09 +0530

    java Assignment

--- assignments/src/assignments/P79.java
+++ assignments/src/assignments/P79.java
@@ -1,10 +1,32 @@
-package assignments;
-
-public class P79 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+// what is the difference between ListIterator and iterator in java
+  Listiterator
+  * List Iterator Traverse in both direction forword and backword.
+  * List Iterator used in list. 
+  * list iterator helps to replace an elemnet.
+  *	Certain methods of ListIterator are next(), previous(), hasNext(), hasPrevious(), add(E e).
+  
+  Iterator
+  * iterator can traverse only in forword direction.
+  * used in collection framework in java .
+  * iterator not helps to iterate an element
+  *  Certain methods of Iterator are next(), remove() and hasNext().
+  
+  eX :
+	  List a=new ArrayList();
+      a.add(10);
+      a.add(12);
+      a.add(14);
+      
+      Iterator i= a.iterator();
+      while(i.hasNext()) {
+    	  system.out.println(i.next())
+      }
+      
+      ListIterator j=a.listIterator();
+      while(j.hasnext()) {
+    	  System.out.println(i.next); //for forword
+      }
+      while(i.hasPrevious()) {
+    	  System.out.println(i.previous());  // for backword
+      }
+  
---
+++ assignments/src/assignments/P80.java
@@ -0,0 +1,9 @@
+// what is the difference between ArrayList an ArrayList<?> in java
+ArrayList 
+   it is raw  type is not type-safe
+   add any type of object
+   
+ ArrayList<?>
+   it is raw type and it is type-Safe.
+   only specified typ is addable
+
